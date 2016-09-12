aac85140794da4035330dfda00f7c439df47d7c4
yuvapriyakumar <yuvamnm@gmail.com>
2016-09-12 19:57:07 +0530

    Update largnum.c

--- largnum.c
+++ largnum.c
@@ -2,10 +2,8 @@
 int main()
 {
     double n1, n2, n3;
-
     printf("Enter three numbers: ");
     scanf("%lf %lf %lf", &n1, &n2, &n3);
-
     if( n1>=n2 && n1>=n3 )
         printf("%.2f is the largest number.", n1);
 
