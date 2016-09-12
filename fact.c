3972dea9856252d96fef21eca987dce6be9d1ca1
yuvapriyakumar <yuvamnm@gmail.com>
2016-09-12 20:29:04 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -3,12 +3,10 @@ int main()
 {
     int n, i;
     unsigned long long factorial = 1;
-
     printf("Enter an integer: ");
     scanf("%d",&n);
     if (n < 0)
         printf("Error! Factorial of a negative number doesn't exist.");
-
     else
     {
         for(i=1; i<=n; ++i)
@@ -17,6 +15,5 @@ int main()
         }
         printf("Factorial of %d = %llu", n, factorial);
     }
-
     return 0;
 }
